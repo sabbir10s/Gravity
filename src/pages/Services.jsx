47de204ab2bdc7b0890f2fcbf4e85fcadb7d7999
alamincoders <developeralamin03@gmail.com>
2023-04-27 13:12:49 +0600

    added services page

---
+++ src/pages/Services.jsx
@@ -0,0 +1,13 @@
+import EveryHero from "../components/shared/EveryHero";
+
+const Services = () => {
+  return (
+    <div>
+      <section className="bg-primary-800 text-white pb-5">
+        <EveryHero>We focus on what we do best providing top-class IT and digital branding services.</EveryHero>
+      </section>
+    </div>
+  );
+};
+
+export default Services;
