f8972cc53954274b10ae3ba306e8d6e8275d14f4
sabbir10s <sabbirahmed1023@gmail.com>
2023-04-27 09:39:56 +0600

    review section added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import FeaturedClients from "../components/screen/FeaturedClients";
 import Hero from "../components/screen/Hero";
 import OurWork from "../components/screen/OurWork";
+import Review from "../components/screen/Review";
 
 const Home = () => {
   return <div>
     <Hero/>
     <OurWork/>
     <FeaturedClients/>
+    <Review/>
   </div>;
 };
 
