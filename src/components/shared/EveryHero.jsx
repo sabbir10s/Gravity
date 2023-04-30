6015f16e3523a18554f43246c94d4a8e3169bf0d
sabbir10s <sabbirahmed1023@gmail.com>
2023-04-30 21:45:00 +0600

    error issu sloved

--- src/components/shared/EveryHero.jsx
+++ src/components/shared/EveryHero.jsx
@@ -1,8 +1,8 @@
 /* eslint-disable react/prop-types */
 const EveryHero = ({ children }) => {
   return (
-    <div className="container h-screen flex items-center">
-      <h2 className="w-full lg:w-5/6 font-medium leading-[125%] lg:leading-[120%] text-[32px]  lg:text-[80px] ">{children}</h2>
+    <div className="container items-center py-36">
+      <h2 className="w-full lg:w-5/6 font-medium leading-[125%] lg:leading-[120%] text-[32px]  lg:text-[60px] ">{children}</h2>
     </div>
   );
 };
