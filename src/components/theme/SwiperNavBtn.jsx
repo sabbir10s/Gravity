fd50cfe2c5a45cf41c00f81db6696ad0a93d9b2a
alamincoders <developeralamin03@gmail.com>
2023-05-01 13:11:32 +0600

    updated review section

---
+++ src/components/theme/SwiperNavBtn.jsx
@@ -0,0 +1,17 @@
+import { useSwiper } from "swiper/react";
+
+const SwiperNavBtn = () => {
+  const swiper = useSwiper();
+  return (
+    <div className=" space-x-4 text-center">
+      <button className="bg-primary-800 hover:bg-primary-600 py-1 px-4 rounded-3xl text-white" onClick={() => swiper.slidePrev()}>
+        Prev
+      </button>
+      <button className="bg-primary-800 hover:bg-primary-600 py-1 px-4 rounded-3xl text-white" onClick={() => swiper.slideNext()}>
+        Next
+      </button>
+    </div>
+  );
+};
+
+export default SwiperNavBtn;
