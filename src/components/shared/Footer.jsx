ea98459f918816094772e82e132684089982e8ac
alamincoders <developeralamin03@gmail.com>
2023-05-06 12:06:47 +0600

    footer update

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -1,50 +1,34 @@
 import { Link } from "react-router-dom";
-import arrow1 from "../../assets/icon/arrow.png";
-import arrow2 from "../../assets/icon/arrow2.png";
-import facebook from "../../assets/icon/facebook.png";
-import linkedIn from "../../assets/icon/linkedIn.png";
-import twitter from "../../assets/icon/twitter.png";
 import logo from "../../assets/img/full logo.png";
 import "../../styles/Footer.css";
 import { routeLinks } from "../../data/RouteLink";
 import FooterButtonLink from "../theme/FooterButtonLink";
+import { FiFacebook, FiLinkedin } from "react-icons/fi";
+import { RiBehanceLine } from "react-icons/ri";
+import { TfiDribbble } from "react-icons/tfi";
 const Footer = () => {
   return (
     <div className="bg-secondary-800 text-white">
       <div className="pt-[60px] lg:pt-[144px] pb-[40px] lg:pb-[80px]">
         <div className="flex items-center justify-between container">
           <div>
-            <h3 className="text-[20px] lg:text-[30px]">Have a project in mind?</h3>
+            <h3 className="text-[20px] lg:text-[54px]">Have a project in mind?</h3>
             <Link to="/contact">
               {" "}
               <button className="text-white group">
-                <h2 className="text-[24px] lg:text-[80px] text-white group-hover:text-primary-800 font-semibold duration-300 transition">
+                <h2 className="text-[24px] lg:text-[76px] text-white group-hover:text-[#6AA0FC] font-semibold duration-300 transition">
                   Let’s connect!
                 </h2>
                 <div className="bg-white w-0 group-hover:w-full duration-300 h-[1px] mx-auto"></div>
               </button>
             </Link>
           </div>
-
-          <div className="relative group block md:hidden">
-            <Link to="/contact">
-              <div className="progress-bar w-16 lg:w-[100px] h-16 lg:h-[100px] rounded-[50%] flex items-center justify-center relative z-10">
-                <div className=" group-hover:rotate-180 duration-300 transition">
-                  <img className=" w-7 lg:w-full" src={arrow1} alt="" />
-                  <img className="absolute top-[3px] left-0 w-7 lg:w-[35px] lg:h-[28px] hidden group-hover:inline-block" src={arrow2} alt="" />
-                </div>
-              </div>
-            </Link>
-            <div className="spinner_footer w-16 lg:w-[100px] h-16 lg:h-[100px] absolute top-0 z-0 hidden group-hover:block">
-              <div className="spinner1_footer w-16 lg:w-[100px] h-16 lg:h-[100px]"></div>
-            </div>
-          </div>
         </div>
 
-        <div className="mt-[40px] lg:mt-[65px] container lg:flex items-center justify-between">
+        <div className="pt-[60px] lg:pt-[120px] container lg:flex items-center justify-between">
           <div>
             <p className="uppercase text-white/40 mb-[12px] lg:mb-[17px] text-[16px] lg:text-[24px]">Email</p>
-            <button className="text-white hover:text-primary-800 duration-300 transition">
+            <button className="text-white hover:text-[#6AA0FC] duration-300 transition">
               <Link className="text-[20px] md:text-[30px] lg:text-[40px]" to="mailto:hello@gravityltd.co">
                 hello@gravityltd.co
               </Link>
@@ -52,7 +36,7 @@ const Footer = () => {
           </div>
           <div className="mt-[24px] lg:mt-0">
             <p className="uppercase text-white/40 mb-[12px] lg:mb-[17px] text-[16px] lg:text-[24px]">Phone Number</p>
-            <button className="text-white hover:text-primary-800 duration-300 transition">
+            <button className="text-white hover:text-[#6AA0FC] duration-300 transition">
               <Link className="text-[20px] md:text-[30px] lg:text-[40px]" to="tel:09638945755">
                 09638-945755
               </Link>
@@ -79,9 +63,18 @@ const Footer = () => {
             <div className="mt-[32px]">
               <p className=" text-white/40 text-[16px] uppercase ">Follow</p>
               <div className="flex items-center gap-[30px] mt-[10px]">
-                <img className="hover:animate-pulse cursor-pointer hover:scale-110 transition duration-150" src={facebook} alt="" />
-                <img className="hover:animate-pulse cursor-pointer hover:scale-110 transition duration-150" src={linkedIn} alt="" />
-                <img className="hover:animate-pulse cursor-pointer hover:scale-110 transition duration-150" src={twitter} alt="" />
+                <Link target="_blank" to="https://www.facebook.com/gravityltd.co/">
+                  <FiFacebook className="footer_icons" />
+                </Link>
+                <Link target="_blank" to="https://www.linkedin.com/company/gravityltd/">
+                  <FiLinkedin className="footer_icons" />
+                </Link>
+                <Link target="_blank" to="https://dribbble.com/gravityltd">
+                  <RiBehanceLine className="footer_icons" />
+                </Link>
+                <Link target="_blank" to="https://www.behance.net/gravity-ltd">
+                  <TfiDribbble className="footer_icons" />
+                </Link>
               </div>
             </div>
           </div>
--- src/components/theme/FooterButtonLink.jsx
+++ src/components/theme/FooterButtonLink.jsx
@@ -3,13 +3,10 @@ import { Link } from "react-router-dom";
 
 const FooterButtonLink = ({ path_name, route }) => {
   return (
-    <button className="text-white hover:text-primary-600 duration-300 group text-left">
+    <button className="text-white hover:text-[#6AA0FC] duration-300 group text-left">
       <Link className="text-xl" to={path_name}>
         {route}
       </Link>
-      {/* <div className="bg-white/20 w-full mx-auto">
-        <div className="bg-white w-0 group-hover:w-full duration-300 h-[1px] mx-auto"></div>
-      </div> */}
     </button>
   );
 };
