2f3ad98c7122fe7ea5ea83bd1f1714cf94e32bad
alamincoders <developeralamin03@gmail.com>
2023-05-06 21:53:07 +0600

    about slider done

--- src/components/screen/About/AboutSlide.jsx
+++ src/components/screen/About/AboutSlide.jsx
@@ -7,16 +7,19 @@ import "swiper/css/pagination";
 
 import { Pagination, Autoplay } from "swiper";
 
-import slider1 from "../../../assets/aboutslide/slider1.png";
-import slider2 from "../../../assets/aboutslide/slider2.png";
-import slider3 from "../../../assets/aboutslide/slider3.png";
+import slider1 from "../../../assets/aboutslide/slider (1).png";
+import slider2 from "../../../assets/aboutslide/slider (2).png";
+import slider3 from "../../../assets/aboutslide/slider (3).png";
+import slider4 from "../../../assets/aboutslide/slider (4).png";
+import slider5 from "../../../assets/aboutslide/slider (5).png";
+import slider6 from "../../../assets/aboutslide/slider (6).png";
 
 const AboutSlide = () => {
   return (
     <div>
       <Swiper
-        spaceBetween={10}
-        slidesPerView={2}
+        spaceBetween={20}
+        slidesPerView={1}
         rewind={true}
         autoplay={{
           delay: 2500,
@@ -24,50 +27,58 @@ const AboutSlide = () => {
         }}
         breakpoints={{
           640: {
-            slidesPerView: 2,
-            spaceBetween: 20,
+            slidesPerView: 1,
+            spaceBetween: 40,
           },
           768: {
             slidesPerView: 2,
-            spaceBetween: 40,
+            spaceBetween: 30,
           },
           1024: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+          },
+          1440: {
             slidesPerView: 3,
-            spaceBetween: 50,
+            spaceBetween: 30,
+          },
+          2560: {
+            slidesPerView: 4,
+            spaceBetween: 30,
           },
         }}
         modules={[Autoplay, Pagination]}
       >
         <SwiperSlide>
-          <div className="w-full h-[230px] lg:w-[616px] 2xl:w-full lg:h-[517px] 2xl:h-[700px]">
+          <div className="about_slide">
             <img loading="lazy" className="w-full h-full object-fill object-center" src={slider1} alt="slider1" />
           </div>
         </SwiperSlide>
         <SwiperSlide>
-          <div className="w-full h-[230px] lg:w-[616px]  2xl:w-full lg:h-[517px] 2xl:h-[700px]">
+          <div className="about_slide">
             <img loading="lazy" className="w-full h-full object-fill object-center" src={slider2} alt="slider2" />
           </div>
         </SwiperSlide>
         <SwiperSlide>
-          <div className="w-full h-[230px] lg:w-[616px] 2xl:w-full lg:h-[517px] 2xl:h-[700px]">
+          <div className="about_slide">
             <img loading="lazy" className="w-full h-full object-fill object-center" src={slider3} alt="slider3" />
           </div>
         </SwiperSlide>
         <SwiperSlide>
-          <div className="w-full h-[230px] lg:w-[616px] 2xl:w-full lg:h-[517px] 2xl:h-[700px]">
-            <img loading="lazy" className="w-full h-full object-fill object-center" src={slider1} alt="slider1" />
+          <div className="about_slide">
+            <img loading="lazy" className="w-full h-full object-fill object-center" src={slider4} alt="slider1" />
           </div>
         </SwiperSlide>
         <SwiperSlide>
-          <div className="w-full h-[230px] lg:w-[616px]  2xl:w-full lg:h-[517px] 2xl:h-[700px]">
-            <img loading="lazy" className="w-full h-full object-fill object-center" src={slider2} alt="slider2" />
+          <div className="about_slide">
+            <img loading="lazy" className="w-full h-full object-fill object-center" src={slider5} alt="slider2" />
           </div>
         </SwiperSlide>
-        <SwiperSlide>
+        {/*   <SwiperSlide>
           <div className="w-full h-[230px] lg:w-[616px] 2xl:w-full lg:h-[517px] 2xl:h-[700px]">
-            <img loading="lazy" className="w-full h-full object-fill object-center" src={slider3} alt="slider3" />
+            <img loading="lazy" className="w-full h-full object-fill object-center" src={slider6} alt="slider3" />
           </div>
-        </SwiperSlide>
+        </SwiperSlide> */}
       </Swiper>
     </div>
   );
