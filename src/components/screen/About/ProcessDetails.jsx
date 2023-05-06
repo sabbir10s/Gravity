03a2fb688e3d328bdf4bfa3cbf24b1bfbe62025f
sabbir10s <sabbirahmed1023@gmail.com>
2023-05-06 20:58:54 +0600

    font size updated

--- src/components/screen/About/ProcessDetails.jsx
+++ src/components/screen/About/ProcessDetails.jsx
@@ -3,8 +3,8 @@ const ProcessDetails = ({process, index}) => {
     return (
         <article>
             <div className="text-secondary-800">
-                <h2 className="text-[#b6c2cf] text-[80px] lg:text-[150px] font-semibold">{index + 1}</h2>
-                <h2 className=" text-2xl lg:text-4xl mb-3 font-semibold">{process.title}</h2>
+                <h2 className="text-[#b6c2cf] text-[80px] lg:text-[130px]">{index + 1}</h2>
+                <h2 className=" text-2xl lg:text-3xl mb-3">{process.title}</h2>
                 <div>
                     <p className="text-base">{process.desc}</p>
                 </div>
