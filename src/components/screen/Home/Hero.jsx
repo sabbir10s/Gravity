03aee084f9e1b81d2fed44d8ad94e4e8bdfb61ef
alamincoders <developeralamin03@gmail.com>
2023-05-06 09:40:33 +0600

    hero updated

--- src/components/screen/Home/Hero.jsx
+++ src/components/screen/Home/Hero.jsx
@@ -1,41 +1,38 @@
+import { TypeAnimation } from "react-type-animation";
 import Video from "./Video";
 const Hero = () => {
+  return (
+    <div className="bg-primary-800 text-white">
+      <div>
+        <div className="container flex items-center pt-[140px] lg:pt-[150px]">
+          <div className="">
+            <h1 className="font-medium text-4xl md:text-[40px] lg:text-[80px] 2xl:text-[100px] lg:leading-[110px]">Ideas Transformed Into</h1>
+            <h2 className="font-medium text-4xl md:text-[40px] lg:text-[80px] 2xl:text-[100px] lg:leading-[110px]">
+              {" "}
+              <TypeAnimation sequence={["Masterpieces", 1000, "Perfection", 1000, "Masterwork", 1000]} repeat={Infinity} />
+            </h2>
 
-    return (
-        <div className="bg-primary-800 text-white">
-            <div>
-                <div className="container flex items-center pt-[140px] lg:pt-[150px]">
-                    <div className="">
-                        <h1
-                            className="font-medium text-3xl md:text-[40px] lg:text-[100px] lg:leading-[110px]">
-                            Ideas transformed into <p className="hidden lg:block"></p> masterpieces
-                        </h1>
-                        <p className="text-[16px] lg:text-[32px] mt-[16px] lg:mt-[24px] md:w-3/4 lg:w-4/5">
-                            Gravity is a digital agency offering IT and digital branding services. We
-                            provide customized solutions to achieve business goals with outstanding results.
-                        </p>
-                    </div>
-                </div>
-
-                <div className="pt-[32px] lg:pt-[60px] pb-[60px] lg:pb-[101px]">
-                    <Video/>
-                </div>
-                <div
-                    className="container lg:flex justify-between items-start pb-[60px]">
-                    <h2 className="text-[20px] md:text-[30px] lg:text-[64px]">About</h2>
-                    <p
-                        className="mt-[12px] lg:mt-0 lg:w-3/4 text-[16px] md:text-[25px] lg:text-[32px]">
-                        Gravity is a fast growing digital agency that specializes in providing
-                        first-rate IT and digital branding services. With a team
-                        of experienced professionals who are passionate about technology and creativity,
-                        Gravity has made a name for itself for providing innovative solutions that help
-                        businesses of all sizes grow and succeed in todays digital landscape.
-                    </p>
-                </div>
-            </div>
-
+            <p className="text-[16px] lg:text-[32px] mt-[16px] lg:mt-[24px] md:w-3/4 lg:w-4/5 ">
+              Gravity is a digital agency offering IT and digital branding services. We provide customized solutions to achieve business goals with
+              outstanding results.
+            </p>
+          </div>
         </div>
-    );
+
+        <div className="pt-[32px] lg:pt-[60px] pb-[60px] lg:pb-[101px]">
+          <Video />
+        </div>
+        <div className="container lg:flex justify-between items-start pb-[60px]">
+          <h2 className="text-[20px] md:text-[30px] lg:text-[40px] font-medium lg:font-semibold">About</h2>
+          <p className="mt-[12px] lg:mt-0 lg:w-3/4 text-[16px] md:text-[25px] text-justify">
+            Gravity is a fast growing digital agency that specializes in providing first-rate IT and digital branding services. With a team of
+            experienced professionals who are passionate about technology and creativity, Gravity has made a name for itself for providing innovative
+            solutions that help businesses of all sizes grow and succeed in todays digital landscape.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
