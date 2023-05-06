45dbe0a300bd8b122a1fca2d7ff6456b4d876dc0
alamincoders <developeralamin03@gmail.com>
2023-05-06 13:29:08 +0600

    updated

--- src/components/screen/services/ServiceDesign.jsx
+++ src/components/screen/services/ServiceDesign.jsx
@@ -6,7 +6,7 @@ const ServiceDesign = ({ serviceDetails }) => {
   const { image, isReverse } = serviceDetails;
 
   return (
-    <section className={`grid grid-cols-1 md:grid-cols-2 lg:gap-x-24 mt-8 lg:mt-28`}>
+    <section className={`grid grid-cols-1 md:grid-cols-2 gap-y-10 lg:gap-x-24 mt-12 lg:mt-40`}>
       <div className={`${isReverse ? "md:order-first" : "md:order-last"}`}>
         <ImageService image={image} />
       </div>
--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -16,14 +16,6 @@ const Services = () => {
       </Helmet>
       <section className="bg-primary-800 text-white pb-5">
         <EveryHero>We focus on what we do best providing top-class IT and digital branding services.</EveryHero>
-        <p className="container md:hidden">
-          <small>
-            {" "}
-            A digital agency is a business that provides services related to digital marketing, web design, web development, and other digital
-            services. A digital agency is usually staffed with a team of experts in various areas of digital marketing and technology, such as SEO,
-            social media marketing, web design and development, content marketing, and more.
-          </small>
-        </p>
       </section>
       <section>
         <Service />
