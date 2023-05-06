701a9e511b71a2674272d9fccb8546d61826083e
sabbir10s <sabbirahmed1023@gmail.com>
2023-05-06 13:59:50 +0600

    horizontal scroll done

--- src/components/screen/About/Process.jsx
+++ src/components/screen/About/Process.jsx
@@ -1,27 +1,24 @@
-import { processData } from "../../../data/ProcessData";
+import {processData} from "../../../data/ProcessData";
 import ProcessDetails from "./ProcessDetails";
-
+import '../../../styles/Process.css'
 const Process = () => {
-  return (
-    <div className="">
-      <div className="container text-secondary-800">
-        <h2 className="text-4xl md:text-5xl lg:text-[64px] font-semibold">
-          A simple and <br /> streamlined process
-        </h2>
-        <div className="w-24 lg:w-52 h-[7px] my-5 bg-primary-600"></div>
-      </div>
-      {/* steps line */}
-      <div className="hidden lg:block steps py-1 bg-[#D9E4EF] mt-24"></div>
-      {/* process description */}
-      <section className="container">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
-          {processData.map((process) => (
-            <ProcessDetails key={process.id} process={process} />
-          ))}
+    return (
+        <div>
+            <div className="container text-secondary-800">
+                <h2 className="text-4xl md:text-5xl lg:text-[64px] font-semibold">
+                    A simple and
+                    <br/>
+                    streamlined process
+                </h2>
+                <div className="w-24 lg:w-52 h-[7px] my-5 bg-primary-600"></div>
+            </div>
+            <div className=" relative h-[40vh] lg:h-[30vh] mt-10 lg:mt-20">
+                <div id="section">
+                    {processData.map((process, index) => (<ProcessDetails key={process.id} process={process} index={index}/>))}
+                </div>
+            </div>
         </div>
-      </section>
-    </div>
-  );
+    );
 };
 
 export default Process;
--- src/components/screen/About/ProcessDetails.jsx
+++ src/components/screen/About/ProcessDetails.jsx
@@ -1,16 +1,18 @@
 /* eslint-disable react/prop-types */
-const ProcessDetails = ({ process }) => {
-  return (
-    <div className="text-secondary-800">
-      <button className="bg-[#D9E4EF] rounded-full inline-block w-14 h-14 text-secondary-800 text-2xl relative lg:-top-8 font-semibold">
-        {process.step}
-      </button>
-      <h2 className=" text-2xl lg:text-3xl my-3 font-semibold">{process.title}</h2>
-      <div>
-        <p>{process.desc}</p>
-      </div>
-    </div>
-  );
+const ProcessDetails = ({process, index}) => {
+    return (
+        <article>
+            <div className="text-secondary-800 items lg:w-[100vh] h-[60vh] lg:h-[100vh] ">
+                <div className="w-[340px] lg:w-full ml-4 lg:container">
+                    <h2 className="text-[#b6c2cf] text-[80px] lg:text-[150px] font-semibold">{index + 1}</h2>
+                    <h2 className=" text-2xl lg:text-4xl my-3 font-semibold">{process.title}</h2>
+                    <div>
+                        <p className="text-base lg:text-lg">{process.desc}</p>
+                    </div>
+                </div>
+            </div>
+        </article>
+    );
 };
 
 export default ProcessDetails;
