6b85b5c7e0648de48170c23566eadee29bb98786
sabbir10s <sabbirahmed1023@gmail.com>
2023-05-09 19:51:49 +0600

    all issues solved

--- src/components/screen/About/Vision.jsx
+++ src/components/screen/About/Vision.jsx
@@ -2,11 +2,11 @@ import visionImg from "../../../assets/aboutslide/vision.png";
 
 const Vision = () => {
   return (
-    <section className="grid md:block lg:block xl:grid gap-8 grid-cols-1 md:grid-cols-2 h-full  container text-secondary-800 relative ">
-      <div className=" pt-8 lg:pt-28 order-2 lg:order-none">
+    <section className="container text-secondary-800 flex flex-col lg:flex-row lg:gap-[130px] ">
+      <div className="pr-0 lg:pr-3 pt-[52px] lg:pt-[100px] order-2 lg:order-none lg:w-2/3">
         <h2 className="text-4xl md:text-5xl lg:text-[64px] font-semibold">Vision</h2>
-        <div className="w-24 h-[7px] my-5 bg-primary-600"></div>
-        <p className="text-base md:text-[20px] lg:text-[24px] xl:text-[22px] 2xl:text-[32px] md:leading-relaxed lg:leading-[48px] text-justify mt-10">
+        <div className="w-24 h-[7px] my-5 bg-primary-800"></div>
+        <p className="text-base md:text-[20px] lg:text-[24px] xl:text-[22px] 2xl:text-[32px] md:leading-relaxed lg:leading-[48px] mt-[24px]">
           Gravity is a dynamic digital agency that specializes in delivering cutting-edge solutions for businesses of all sizes. With a team of
           skilled professionals and a focus on innovation and excellence, Gravity helps its clients achieve their digital goals and take their brands
           to new heights. From web design and development to branding and marketing, Gravity offers a comprehensive range of services that are
@@ -14,8 +14,8 @@ const Vision = () => {
           businesses succeed in the ever-changing digital landscape.
         </p>
       </div>
-      <div className="lg:mt-10 xl:mt-0 w-full h-full xl:w-[733px] 2xl:w-[1000px] lg:h-[800px] xl:h-[813px] 2xl:h-[1000px] order-1 lg:order-none pt-12 md:pt-0  rounded lg:rounded-none xl:absolute top-0 lg:-right-52 xl:-right-60 2xl:-right-[720px]  block md:hidden lg:block">
-        <img className="w-full h-full rounded lg:rounded-none" src={visionImg} alt="vision" />
+      <div className="mt-[55px] lg:mt-0">
+        <img className="w-full h-full object-cover object-center rounded lg:rounded-none" src={visionImg} alt="vision" />
       </div>
     </section>
   );
