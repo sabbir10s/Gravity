8d698027f283fecd1304dad45c014765ccd1c7b0
alamincoders <developeralamin03@gmail.com>
2023-05-10 13:09:01 +0600

    updated

--- src/components/screen/About/Vision.jsx
+++ src/components/screen/About/Vision.jsx
@@ -7,7 +7,7 @@ const Vision = () => {
         <div className="pr-0 lg:pr-3 pt-[52px] lg:pt-[100px] order-2 lg:order-none lg:w-2/3">
           <h2 className="text-4xl md:text-5xl lg:text-[64px] font-semibold">Vision</h2>
           <div className="w-14 lg:w-24 h-[7px] mb-[30px] lg:mb-[50px] mt-[5px] lg:mt-[9px] bg-primary-800"></div>
-          <p className="text-base md:text-[18px] lg:text-[20px] xl:text-[22px] 2xl:text-[28px] md:leading-relaxed lg:leading-[48px] mt-[24px]">
+          <p className="text-base md:text-[18px] lg:text-[20px] xl:text-[22px] 2xl:text-[28px] md:leading-relaxed lg:leading-[48px] xl:leading-loose mt-[24px] lg:mr-40">
             Gravity is a dynamic digital agency that specializes in delivering cutting-edge solutions for businesses of all sizes. With a team of
             skilled professionals and a focus on innovation and excellence, Gravity helps its clients achieve their digital goals and take their
             brands to new heights. From web design and development to branding and marketing, Gravity offers a comprehensive range of services that
@@ -16,7 +16,7 @@ const Vision = () => {
           </p>
         </div>
 
-        <div className="mt-[55px] lg:mt-0 w-full h-full lg:w-1/3 lg:absolute right-0">
+        <div className="mt-[55px] lg:mt-0 w-full h-full lg:w-[40%] lg:absolute right-0">
           <img className="w-full h-full object-cover object-center rounded lg:rounded-none" src={visionImg} alt="vision" />
         </div>
       </div>
